bfb9d9df71927fd6d2736fcd81cdaa007e0080ac
Koorosh <cyrusasil04@gmail.com>
2023-08-07 13:53:51 +0430

    added 4 inlinekeyboard buttons

--- codingconfig.py
+++ codingconfig.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv , dotenv_values
 
 load_dotenv()
-BOT_TOKEN = (os.getenv("BOT_TOKEN"))
+BOT_TOKEN = "6499678561:AAHxLJ2DSGO0SZJKYjzJ9LTaKO0dHDeIJz8"
+
 import telebot
 
 bot = telebot.TeleBot(BOT_TOKEN)
