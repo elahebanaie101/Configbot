aa80dd643597b0f030177a5b657494ceb27ec142
elahebanaie101 <e.banaie1223@gmail.com>
2023-08-12 14:55:55 +0000

    closes #1

--- init.bash
+++ init.bash
@@ -2,6 +2,6 @@ if [ -d "/etc/pandora" ]; then
   exit 1 ; 
 fi
 mkdir /etc/pandora ; 
-echo "{}" > /etc/pandora/v2ray_config.json ; 
+cp v2ray-linux-64/vpoint_vmess_freedom.json /etc/pandora/v2ray_config.json ; 
 echo "[]" > /etc/pandora/accounts.json ; 
 echo '{"telegram_bot_token":"","expiration_check_interval":30}' > /etc/pandora/app_config.json ; 
